51384f90a0f8721b48a6fa1afb3f1cb0d224366f
Noval Maulana <164361509+NovalMaulana@users.noreply.github.com>
2025-04-18 00:38:58 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,25 +10,15 @@ app.use(cors());
 
 // Inisialisasi GoogleAuth dengan credentials dari environment variable
 let auth;
-console.log('Checking for GOOGLE_APPLICATION_CREDENTIALS_JSON:', !!process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
-if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-  try {
-    const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
-    console.log('Successfully parsed credentials from environment variable');
-    auth = new GoogleAuth({
-      credentials,
-      scopes: ['https://www.googleapis.com/auth/drive'],
-    });
-  } catch (error) {
-    console.error('Error parsing credentials from environment variable:', error);
-    throw error;
-  }
-} else {
-  console.log('Using keyFile instead of environment variable');
+try {
+  const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
   auth = new GoogleAuth({
-    keyFile: './service-account.json',
+    credentials,
     scopes: ['https://www.googleapis.com/auth/drive'],
   });
+} catch (error) {
+  console.error('Error initializing GoogleAuth:', error);
+  throw error;
 }
 
 // Inisialisasi Drive dengan auth
